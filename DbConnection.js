1a7f3026cb9dbf5c3dee8432ef800109d82e01e4
Pratik Ahire <73336913+pratikahire1299@users.noreply.github.com>
2021-11-04 17:54:22 +0530

    Add files via upload

--- DbConnection.js
+++ DbConnection.js
@@ -5,8 +5,10 @@ var mongo = require('mongodb');
 var MongoClient = mongo.MongoClient;
 const mongoose=require('mongoose');
 
-uri="mongodb://localhost:27017/cricketdb";
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>console.log("Connection Successful")).catch((err)=>console.log("Failed"));
+uri="mongodb://localhost:27017/Instagram_lite_db";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+                                .then(()=>console.log("Connection Successful: DataBase Name:Instagram_lite_db"))
+                                .catch((err)=>console.log("Failed"));
 
 
 
--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const  Express =require("express");
-const jst=require("jsonwebtoken");
-
+const jwt=require("jsonwebtoken");
+require("dotenv").config();
+const bcrypt=require("bcrypt");
 const homepage=require("./homepage.js");
 const userdashboard=require("./userdashboard.js");
 const dbconnection=require("./DbConnection.js");
+const User = require("./DatabaseSchemas/userdetails");
 
 const Port= process.env.Port || 2000;
 
 /*const createToken = async()=>{
-    const token=await jwt.sign({_id:101},"secretkey","thesecretkeyformyprojectcalledminiinstagramapplication")
+    const token=await jwt.sign({_id:101},"secretkey","thisistheecretkeyformyprojectcalledminiinstagramapplication")
     console.log(token);
 }
 
@@ -16,8 +18,10 @@ createToken();
 
 */
 
-const app=Express();
+const TOKEN_KEY="thisistheecretkeyformyprojectcalledminiinstagramapplication";
 
+const app=Express();
+app.use(Express.json());
 
 app.use('/homepage',homepage);
 app.use('/userdashboard',userdashboard)
@@ -27,4 +31,57 @@ app.listen(Port,()=>{
 })
 
   
+// Register
+app.post("/register", async (req, res) => {
+
+    try {
+
+      const { Name, User_id, Contact_Number, Age,Password } = req.body;
+  
+  
+      //if (!(User_id && Password && Name && Contact_Number && Age)) {
+       // res.status(400).send("All input is required");
+     // }
+  
+  
+      const oldUser = await User.findOne({ User_id });
+  
+      if (oldUser) {
+        return res.status(409).send("User Already Exist. Please Login");
+      }
+  
+    
+      encryptedPassword = await bcrypt.hash(Password, 10);
+  
+      const user = await User.create({
+        Name,
+        User_id,
+        Contact_Number, 
+        Age,
+        Password: encryptedPassword,
+      });
+     
+    
+      
+  
+      const token = jwt.sign( User_id , TOKEN_KEY, {
+        algorithm: "HS256",
+      })
+      console.log("token:", token)
+  
+    
+      user.token = token;
+  
+      res.status(201).json(user);
+    } catch (err) {
+      console.log(err);
+    }
+    
+  });
+    
+    // Login
+
+
+
+
 module.exports = app;
