bc5d9070efdcac7d389e0f019a7a2bdb7efededd
Pratik Ahire <73336913+pratikahire1299@users.noreply.github.com>
2021-11-09 18:38:25 +0530

    Add files via upload

---
+++ models/userdetails.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const User = new mongoose.Schema({
+	Name: {type: String, default:''},
+	User_id: {type: String, default:''},
+	Contact_Number: {type: Number, default:0},
+	Age: {type: Number, default:0},
+	//Birthdate:{type: "string",format: "date"},
+	token: { type: String },
+	Password: {type:String}
+})
+
+module.exports = mongoose.model('User', User)
