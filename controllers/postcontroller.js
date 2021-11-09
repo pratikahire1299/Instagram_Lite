9e0c6ee5c2daa0dc045b7f2e46a1bcabcf4bbb1f
Pratik Ahire <73336913+pratikahire1299@users.noreply.github.com>
2021-11-09 18:39:24 +0530

    Add files via upload

---
+++ controllers/postcontroller.js
@@ -0,0 +1,75 @@
+const posts = require('../models/postdetails')
+
+module.exports = {
+
+	get: (params) => {
+		return new Promise((resolve, reject) => {
+			posts.find(params)
+			.then(data => {
+				resolve(data)
+			})
+			.catch(err => {
+				reject(err)
+			})
+		})
+	},
+
+	getById: (id) => {
+		return new Promise((resolve, reject) => {
+			posts.findById(id)
+			.then(data => {
+				resolve(data)
+			})
+			.catch(err => {
+				reject(new Error('Post ' + id +' not found'))
+			})
+		})
+	},
+
+	post: (params) => {
+		return new Promise((resolve, reject) => {
+			// hash PW
+			posts.create(params)
+			.then(data => {
+				resolve(data)
+			})
+			.catch(err => {
+				reject(err)
+			})
+		})
+	},
+
+	put: (id, params) => {
+		return new Promise((resolve, reject) => {
+			posts.findByIdAndUpdate(id, params, {new:true})
+			.then(data => {
+				resolve(data)
+			})
+			.catch(err => {
+				reject(err)
+			})
+		})
+	},
+
+	delete: (id) => {
+		return new Promise((resolve, reject) => {
+			posts.findByIdAndRemove(id)
+			.then(() => {
+				resolve({id: id})
+			})
+			.catch(err => {
+				reject(err)
+			})
+		})
+	}
+}
+
+
+
+
+
+
+
+
+
+
