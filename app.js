ed0fea79f0a97cffa3715d5eb8ca57f93087feda
Pratik Ahire <73336913+pratikahire1299@users.noreply.github.com>
2021-11-09 18:36:16 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const  Express =require("express");
 const jwt=require("jsonwebtoken");
 require("dotenv").config();
 const bcrypt=require("bcrypt");
-const homepage=require("./homepage.js");
-const userdashboard=require("./userdashboard.js");
+const homepage=require("./routes/homepage.js");
+const userdashboard=require("./routes/userdashboard.js");
 const dbconnection=require("./DbConnection.js");
-const User = require("./DatabaseSchemas/userdetails");
+const User = require("./models/userdetails");
+const auth = require("./Middleware/authentication.js");
 
 const Port= process.env.Port || 2000;
 
@@ -23,12 +24,10 @@ const TOKEN_KEY="thisistheecretkeyformyprojectcalledminiinstagramapplication";
 const app=Express();
 app.use(Express.json());
 
-app.use('/homepage',homepage);
+
 app.use('/userdashboard',userdashboard)
 
-app.listen(Port,()=>{
-    console.log("App Serever Started");   
-})
+
 
   
 // Register
@@ -37,13 +36,6 @@ app.post("/register", async (req, res) => {
     try {
 
       const { Name, User_id, Contact_Number, Age,Password } = req.body;
-  
-  
-      //if (!(User_id && Password && Name && Contact_Number && Age)) {
-       // res.status(400).send("All input is required");
-     // }
-  
-  
       const oldUser = await User.findOne({ User_id });
   
       if (oldUser) {
@@ -82,39 +74,50 @@ app.post("/register", async (req, res) => {
     // Login
     app.post("/login", async (req, res) => {
 
-      // Our login logic starts here
       try {
-
-        const { User_id, Password } = req.body;
-    
   
+        const { User_id, Password } = req.body;
         if (!(User_id && Password)) {
           res.status(400).send("All input is required");
         }
-
         const user = await User.findOne({ User_id });
     
         if (user && (await bcrypt.compare(Password, user.Password))) {
-         
+
           const token = jwt.sign( User_id , TOKEN_KEY, {
             algorithm: "HS256",
           })
           console.log("token:", token)
+   
           user.token = token;
     
-         
           res.status(200).json(user);
         }
         res.status(400).send("Invalid Credentials");
       } catch (err) {
         console.log(err);
       }
-    
+      
     });
     
     // ...
 
 
 
+app.get("/welcome", auth, (req, res) => {
+  res.status(200).send("Welcome ");
+});
+
+// app.get("/homepage", auth, (req, res) => {
+//   res.status(200).send("Homepage");
+// //  res.render('/homepage',homepage);
+//    app.use('/homepage',homepage);
+// });
+
+app.use('/homepage',auth,homepage);
+
+app.listen(Port,()=>{
+  console.log("App Serever Started");   
+})
 
 module.exports = app;
