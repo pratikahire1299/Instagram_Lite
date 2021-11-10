7f4be43f85d8d7150752b0356a70ee5d76758397
Pratik Ahire <73336913+pratikahire1299@users.noreply.github.com>
2021-11-10 19:37:19 +0530

    Add files via upload

--- routes/homepage.js
+++ routes/homepage.js
@@ -2,41 +2,11 @@ const  Express =require("express");
 const router=Express.Router();
 const controllers = require('../controllers/postcontroller')
 const posts=require("../models/postdetails");
-router.get('/', (req, res) => {
-const data="123";
-    console.log("This is Homepage");
-	if (data == null){
-		res.json({
-			confirmation: 'fail',
-			message: 'Invalid request. Resource undefined.'
-		})
+const auth = require("../Middleware/authentication.js"); 
 
-		return
-	}
 
-	// res.json({
-	// 	confirmation: 'success',
-	// 	Status_code: "200",
-    //     data:"data"
-        
-	// })
 
-    const readposts=async()=>{
-        try{
-            const result=await posts.find();
-            //console.log(result);
-            res.json({
-                confirmation: 'success',
-                Status_code: "200",
-                data:result
-                
-            })
-        }catch(err){
-            console.log(err);
-        }
-    }
-    readposts();
-})
+router.get("/", auth, controllers.get_all_posts);
 
 
 
--- routes/userdashboard.js
+++ routes/userdashboard.js
@@ -1,5 +1,13 @@
 const  Express =require("express");
 const router=Express.Router();
+const controllers = require('../controllers/postcontroller')
+const users=require("../models/postdetails");
 
+const auth = require("../Middleware/authentication.js"); 
+
+
+router.post("/", auth, controllers.create_user_post);
+router.delete("/:User_id", auth, controllers.delete_user_post);
+router.get("/:postid",auth, controllers.get_user_posts);
 
 module.exports = router;
