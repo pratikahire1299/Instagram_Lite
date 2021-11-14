35f154570a0c277835705042efdea2e6b7d4506f
Pratik Ahire <73336913+pratikahire1299@users.noreply.github.com>
2021-11-14 19:08:21 +0530

    Add files via upload

--- routes/userdashboard.js
+++ routes/userdashboard.js
@@ -5,9 +5,10 @@ const users=require("../models/postdetails");
 
 const auth = require("../Middleware/authentication.js"); 
 
-router.get("/:postid",auth, controllers.get_user_posts);
-router.put("/:User_id", auth, controllers.create_user_post);
-router.delete("/:User_id", auth, controllers.delete_user_post);
-router.patch("/:User_id",auth,controllers.Update_User_post);
+router.get("/:postid",auth, controllers.get_user_posts);   // Get posts of User
+router.put("/:User_id", auth, controllers.create_user_post); //ADD Post of User
+router.delete("/:User_id", auth, controllers.delete_all_user_post);  // delete all posts of User
+router.put("/:User_id/:date", auth, controllers.delete_user_post); // delete specific post according to data 
+router.patch("/:User_id/:date",auth,controllers.Update_User_post); //Update any post according to date
 
 module.exports = router;
