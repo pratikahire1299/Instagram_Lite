904142d613b04939517aa09336ee1c497324a35a
Pratik Ahire <73336913+pratikahire1299@users.noreply.github.com>
2021-11-20 15:11:59 +0530

    Add files via upload

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,73 +1,95 @@
-
-module.exports = {
-
-	get: (params) => {
-		return new Promise((resolve, reject) => {
-			posts.find(params)
-			.then(data => {
-				resolve(data)
-			})
-			.catch(err => {
-				reject(err)
-			})
-		})
-	},
-
-	getById: (id) => {
-		return new Promise((resolve, reject) => {
-			posts.findById(id)
-			.then(data => {
-				resolve(data)
-			})
-			.catch(err => {
-			})
-		})
-	},
-
-	post: (params) => {
-		return new Promise((resolve, reject) => {
-			// hash PW
-			posts.create(params)
-			.then(data => {
-				resolve(data)
-			})
-			.catch(err => {
-				reject(err)
-			})
-		})
-	},
-
-	put: (id, params) => {
-		return new Promise((resolve, reject) => {
-			posts.findByIdAndUpdate(id, params, {new:true})
-			.then(data => {
-				resolve(data)
-			})
-			.catch(err => {
-				reject(err)
-			})
-		})
-	},
-
-	delete: (id) => {
-		return new Promise((resolve, reject) => {
-			posts.findByIdAndRemove(id)
-			.then(() => {
-				resolve({id: id})
-			})
-			.catch(err => {
-				reject(err)
-			})
-		})
-	}
-}
+const userdetails = require('../models/userdetails')
+const mongoose = require("mongoose");
 
 
 
+exports.get_all_users = (req, res, next) => {
+	userdetails.find()
+	    .then(docs => {
+		res.status(200).json({
+		  posts_count: docs.length,
+		  All_Posts: docs.map(doc => {
+			return {
+			  _id: doc._id,
+			  User_Id: doc.User_id,
+			  Contact_Number: doc.Contact_Number,
+			  Age: doc.Age
+			};
+		  })
+		});
+	  })
+	  .catch(err => {
+		res.status(500).json({
+		  error: err
+		});
+	  });
+  };
 
+  exports.get_user_data = (req, res, next) => {
+	const id = req.params.User_id;
+	userdetails.findById(id)
+	  .select("_id User_id Contact_Number Age")
+	  .exec()
+	  .then(doc => {
 
+		if (doc) {
+		  res.status(200).json({
+			_id: doc._id,
+			User_Id: doc.User_id,
+			Contact_Number:doc.Contact_Number,
+			Age: doc.Age
+	
+		  });
+		} else {
+		  res
+			.status(404)
+			.json({ message: "No valid User found for provided ID" });
+		}
+	  })
+	  .catch(err => {
+		console.log(err);
+		res.status(500).json({ error: err });
+	  });
+  };
 
+ 
 
+  
+  exports.Update_User_data = (req, res, next) => {
+	const id = req.params.User_id;
+	const { Name, User_id, Contact_Number, Age, Password } = req.body;
 
-
-
+		userdetails.update(
+		 {_id: id },
+		 { $set: {Name : Name,User_id:User_id,Contact_Number:Contact_Number,Age:Age} },
+		 {multi:true}
+	     )
+	  .exec()
+	  .then(result => {
+		res.status(200).json({
+		  message: "User Details updated",
+		});
+	  })
+	  .catch(err => {
+		console.log(err);
+		res.status(500).json({
+		  error: err
+		});
+	  });
+  };
+  
+  exports.delete_user = (req, res, next) => {
+	userdetails.deleteOne({ _id: req.params.User_id })
+	  .exec()
+	  .then(result => {
+		res.status(200).json({
+		  message: "User deleted",
+		  
+		});
+	  })
+	  .catch(err => {
+		res.status(500).json({
+		  error: err
+		});
+	  });
+  };
