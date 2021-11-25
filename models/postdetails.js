26870a2ababde3ba4d2cc798c649d0b3e3f9b47e
Pratik Ahire <73336913+pratikahire1299@users.noreply.github.com>
2021-11-25 18:34:57 +0530

    Add files via upload

--- models/postdetails.js
+++ models/postdetails.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 
 const Postdata = new mongoose.Schema({
-    User_id: {type: String, default:''},
-	Heading: {type: String, default:''},	
+
+    User_id:{ type: mongoose.Schema.Types.ObjectId},
+    User_Name: {type: String, default:''},
+    Heading: {type: String, default:''},	
 	Description: {type: String, default:'None'},
     LastModifiedDate:{type: "string",default: "None"}
+ 
 })
 
 module.exports = mongoose.model('Postdata', Postdata)
--- models/userdetails.js
+++ models/userdetails.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose')
 
 const User = new mongoose.Schema({
 	Name: {type: String, default:''},
-	User_id: {type: String, default:''},
+	User_Name: {type: String, default:''},
 	Contact_Number: {type: Number, default:0},
-	Age: {type: Number, default:0},
-	//Birthdate:{type: "string",format: "date"},
-	token: { type: String },
+	Birthdate:{type: "string",format: "date"},
 	Password: {type:String}
 })
 
