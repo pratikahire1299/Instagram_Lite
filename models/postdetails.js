e27894871658d0930652ea1c068a9e018637a230
Pratik Ahire <73336913+pratikahire1299@users.noreply.github.com>
2021-11-29 17:57:05 +0530

    Add files via upload

--- models/postdetails.js
+++ models/postdetails.js
@@ -6,8 +6,8 @@ const Postdata = new mongoose.Schema({
     User_Name: {type: String, default:''},
     Heading: {type: String, default:''},	
 	Description: {type: String, default:'None'},
-    LastModifiedDate:{type: "string",default: "None"}
- 
+    LastModifiedDate:{type: "string",default: "None"},
+    ImageOfPost:{type:"string"}
 })
 
 module.exports = mongoose.model('Postdata', Postdata)
