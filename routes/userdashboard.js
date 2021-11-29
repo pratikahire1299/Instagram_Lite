d5cdc3c677052f7155e0b860664f09943cf6f58d
Pratik Ahire <73336913+pratikahire1299@users.noreply.github.com>
2021-11-29 20:02:08 +0530

    Add files via upload

--- routes/userdashboard.js
+++ routes/userdashboard.js
@@ -38,9 +38,9 @@ const auth = require("../Middleware/authentication.js");
 
 router.get("/:User_id",auth, controllers.get_user_posts);   // Get posts of User Accept:user: _id
 
-router.post("/:User_id", auth, upload.single('productImage'), controllers.create_user_post); //ADD Post of User Accept:user: _id
+router.post("/:User_id", auth, upload.single('ImageOfPost'), controllers.create_user_post); //ADD Post of User Accept:user: _id
 router.delete("/:Post_id", auth, controllers.delete_user_post); // delete  post of User required Accept: post: _id
-router.patch("/:Post_id",auth,controllers.Update_User_post); //Update any post  Accept: Post: _id  
+router.patch("/:Post_id",auth, upload.single('ImageOfPost'),controllers.Update_User_post); //Update any post  Accept: Post: _id  
 router.delete("/:User_id/:all", auth, controllers.delete_all_user_post);  // delete  post of User required Accept: user: _id
 
 // router.post('/:User_id',auth, upload.single('productImage'), function (req, res, next) {
