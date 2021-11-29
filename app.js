4e69f08dbd57ae4c8fa240e4094153014ef8925d
Pratik Ahire <73336913+pratikahire1299@users.noreply.github.com>
2021-11-29 17:55:31 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const Express = require("express");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const bcrypt = require("bcrypt");
+var multer=require('multer');
+var upload=multer({dest:'public/uploads/'});
+
 
 const homepage = require("./routes/homepage.js");
 const userdashboard = require("./routes/userdashboard.js");
@@ -26,6 +29,14 @@ app.use('/homepage', auth, homepage);
 app.use('/userdashboard', auth, userdashboard);
 app.use('/userprofile',userprofile);
 app.use('/loginRegisterPage',loginRegister);
+
+app.post('/add', upload.single('productImage'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  console.log(req.body.sample);
+  console.log(req.file)
+  
+})
 
 
 app.listen(Port, () => {
